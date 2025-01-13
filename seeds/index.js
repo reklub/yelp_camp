814693e65b17750d56de0df18e8ce01970c5dfc5
slewandowski <s.lewandowski@interia.pl>
2025-01-13 13:35:36 +0100

    added partials

--- seeds/index.js
+++ seeds/index.js
@@ -21,14 +21,16 @@ const seedDb = async () => {
     await Campground.deleteMany({});
     for (let i=0; i<50; i++) {
         const random1000 = Math.floor(Math.random() *1000);
+        const price = Math.floor(Math.random() *20)+10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas modi consequuntur animi dolore. Ratione, unde. Earum aliquid dolores facere architecto voluptates maxime cupiditate, odio quibusdam, aspernatur iusto nostrum totam accusamus.',
+            price
         })
         await camp.save();
     }
-    /* const c = new Campground ({title: 'purple field'});
-    await c.save(); */
 }
 
 seedDb().then ( () => {
